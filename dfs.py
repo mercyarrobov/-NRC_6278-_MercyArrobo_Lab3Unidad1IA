0a7435812bad949fa171191a7056d4c404bff262
mercyarrobov <myarrobo@espe.edu.ec>
2022-05-30 01:44:52 -0500

    Función agregar arista

--- dfs.py
+++ dfs.py
@@ -44,3 +44,34 @@ class Grafo:
                 m_list_ady : mutable
                     Defino el diccionario para implementar la lista adyacencia
         '''
+    #Número de aristas
+        self.m_num_of_nodes = numeros_de_nodos   
+        #Inicializa el rango de los nodos
+        self.m_nodos = range(self.m_num_of_nodes)
+        #Definir el tipo de un grafo dirigido o no dirigido
+        self.m_directo = directo 
+        # Lista de adyacencia usando diccionario
+        self.m_lista_ady = {nodo: set() for nodo in self.m_nodos}
+        	
+    def agregar_aristas(self, nodo1, nodo2, peso=1):
+        
+        """
+        Función que agrega una arista al grafo
+
+        Parámetros
+        ----------
+        nodo1 : int
+            nodo de partida
+        nodo2 : int
+            nodo de llegada
+        peso : int
+            peso del nodo
+            
+        """
+        #Añade la arista del nodo1 al nodo2"""
+        self.m_lista_ady[nodo1].add((nodo2, peso))
+
+        #Si un grafo es no dirigido, añade la misma arista
+        if not self.m_directo:
+            #Pero también en la dirección opuesta
+            self.m_lista_ady[nodo2].add((nodo1, peso))
